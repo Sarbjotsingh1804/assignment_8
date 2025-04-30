b7385ccc8044a727420536eb3f0db378707aeb77
Sarbjot singh <120565238+Sarbjotsingh1804@users.noreply.github.com>
2025-04-30 18:59:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://sarbjotsingh1804:Sarbjot1804@cluster0.c1dxyqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true");
+mongoose.connect("mongodb+srv://sarbjotsingh1804:sarbjotsingh1804@@>@cluster0.dmbkomj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 const trySchema = new mongoose.Schema({
     name: String,
 });
