88a76329813c4ae817d791e3f1c3f92386c07441
Sarbjot singh <120565238+Sarbjotsingh1804@users.noreply.github.com>
2025-04-30 19:28:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,31 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sarbjotsingh1804:sarbjotsingh1804@cluster0.gban9di.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://sarbjotsingh1804:sarbjotsingh1804@@>@cluster0.dmbkomj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 const trySchema = new mongoose.Schema({
     name: String,
 });
